c304a9d6b00e3e263baa2bc046140d57e50c483a
kyrellr <kyrellr@gmail.com>
2022-11-14 19:36:51 -0800

    update main.js for part2 explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,15 +45,32 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ('serviceWorker' in navigator) {
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', func);
+
+    // Steps B3-B6 will be *inside* the event listener's function created in B2
+    function func() {
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+      try {
+        const registration = navigator.serviceWorker.register('./sw.js');
+
+        // B4. TODO - Once the service worker has been successfully registered, console
+        //            log that it was successful.
+        if (registration.active) {
+          console.log("Service worker successfully registered.");
+        }
+      }
+        // B5. TODO - In the event that the service worker registration fails, console
+        //            log that it has failed.
+        catch (error) {
+          console.error(`Service worker registration failed with ${error}`);
+        }
+    }
+    
+    // STEPS B6 ONWARDS WILL BE IN /sw.js
+  }
 }
 
 /**
